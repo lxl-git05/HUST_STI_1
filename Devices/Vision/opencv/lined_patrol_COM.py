9f90ca4086b75961a50b018a967bf22eb877f95c
Phoenix15214 <2198361886@qq.com>
2025-10-30 05:55:19 +0000

    Vision-COM-change

--- Devices/Vision/opencv/lined_patrol_COM.py
+++ Devices/Vision/opencv/lined_patrol_COM.py
@@ -17,7 +17,7 @@ if not cap.isOpened():
     print("错误：无法打开摄像头")
     exit()
 
-pack = SerialPacket(port="COM1", baudrate=115200, timeout=0.1)
+pack = SerialPacket(port="/dev/ttyUSB0", baudrate=115200, timeout=0.1)
 
 windowname = 'window'
 cv2.namedWindow(windowname)
@@ -34,20 +34,23 @@ try:
         roi = frame[height // 2 - 120:height // 2 + 120, width // 2 - 160:width // 2 + 160]
         angel = ls(roi)
         x, y, roi, isj = get_center_point(roi)
-        print(angel)
-        print(count_black_pixels_at_y(roi,120))
+        cv2.circle(roi, (x,y), 33, (255,0,0), -1)
+        #print(angel)
+        #print(count_black_pixels_at_y(roi,120))
         if(count_black_pixels_at_y(roi,120)>200):
             isj = 2
         # x = int(x*255/640)
         cv2.imshow(windowname, roi)
         if cv2.waitKey(1) & 0xFF == 27:
             break
-        print(f"x: {x},y: {y}, isj: {isj}")
+        # print(f"x: {x},y: {y}, isj: {isj}")
         angel=int(angel)+100
+        num = 0x06
+        pack.insert_byte(num)
         pack.insert_two_bytes(pack.num_to_bytes(x+100))
         pack.insert_two_bytes(pack.num_to_bytes(isj))
         pack.insert_two_bytes(pack.num_to_bytes(angel+100))
-        # pack.send_packet()
+        pack.send_packet()
 
     # cv2.imshow('frame',frame)
     # time.sleep(0.01)
