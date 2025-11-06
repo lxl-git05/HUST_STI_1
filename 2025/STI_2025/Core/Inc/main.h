61e8cc608fcf43dbd2af1e22c4f635fa18c0ac65
lxl-git05 <XinLiu051024@outlook.com>
2025-11-06 15:55:19 +0800

    PCB-OLED引脚翻转,代码部分翻转

--- 2025/STI_2025/Core/Inc/main.h
+++ 2025/STI_2025/Core/Inc/main.h
@@ -71,10 +71,10 @@ void Error_Handler(void);
 #define Motor_A_IN1_GPIO_Port GPIOA
 #define Motor_B_IN1_Pin GPIO_PIN_11
 #define Motor_B_IN1_GPIO_Port GPIOA
-#define OLED_SCL_Pin GPIO_PIN_8
-#define OLED_SCL_GPIO_Port GPIOB
-#define OLED_SDA_Pin GPIO_PIN_9
+#define OLED_SDA_Pin GPIO_PIN_8
 #define OLED_SDA_GPIO_Port GPIOB
+#define OLED_SCL_Pin GPIO_PIN_9
+#define OLED_SCL_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 void HAL_SYSTICK_Callback(void) ;
