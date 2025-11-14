f8e31aa672275d9ccee6765b90b6b1077b3fe59e
lxl-git05 <XinLiu051024@outlook.com>
2025-11-15 00:12:05 +0800

    红绿灯系列程序-README说明书完成

--- 2025/STI_2025_RGB/Control/Mymain.c
+++ 2025/STI_2025_RGB/Control/Mymain.c
@@ -1,27 +1,18 @@
 #include "Mymain.h"
 
-// 要求:
+// 要求(更多相关信息见README):
 /*
 	1. RGB灯:(可开关)
-	1.1 自动档: 每个周期3s, 红灯3s - 黄灯3s - 绿灯3s
-	1.2 手动挡: 可选择RGB颜色
+	1.1 自动档(1): 每个周期3s, 红灯3s - 黄灯3s - 绿灯3s
+	1.2 手动挡(0): 可选择RGB颜色
 
 	2. 指示牌
-	2.1 自动档: 每个周期5s, R 5s - L 5s
-	2.2 手动档: 可任意选择指示牌的方向: R / L
+	2.1 自动档(1): 每个周期5s, R 5s - L 5s
+	2.2 手动档(0): 可任意选择指示牌的方向: R / L
 
 	3. 蓝牙
 	实现控制切换这两个模式各自的手动挡和自动档
 */
-
-// *******************全局变量*******************
-
-
-// *******************实验区域变量*******************
-// Debug调试参数
-int check1 = 1;
-int check2 ;
-int check[50] = {0};
 
 void Mymain(void)
 {
@@ -36,17 +27,15 @@ void Mymain(void)
 		// 全部初始化完毕后再开启Systick中断
 		__enable_irq();	
 	}
-	// *******************实验区域*******************
-	
+
 	while(1)
 	{
 		// ******************* while *******************
 		BLE_Data_Update() ;					// 蓝牙接收模式信息
 		RGB_Control(RGB_Mode) 	  ;	// RGB模式
 		Servo_Control(Servo_Mode) ;	// 舵机模式
-		// ******************* 实验区域 *******************
-		Menu_Display() ;
-		Menu_RGB_Servo_Manu_Update(RGB_Mode , Servo_Mode , &RGB_Manu_Num , &Servo_Manu_Num ) ;
+		Menu_Display() ;						// OLED菜单显示
+		Menu_RGB_Servo_Manu_Update(RGB_Mode , Servo_Mode , &RGB_Manu_Num , &Servo_Manu_Num ) ;	// 菜单反馈,实现手动调控参数
 		
 		OLED_Update() ;	// 必须存在:OLED更新
 	}
@@ -55,9 +44,9 @@ void Mymain(void)
 // Systick定时中断,1ms周期
 void HAL_SYSTICK_Callback(void)
 {
-	// 计时
-	static int count_sys = 0 ;
-	count_sys ++ ;
+//	// 计时
+//	static int count_sys = 0 ;
+//	count_sys ++ ;
 	// 功能1: 按键
 	Key_Tick() ;
 	// 功能2:RGB自动档函数
