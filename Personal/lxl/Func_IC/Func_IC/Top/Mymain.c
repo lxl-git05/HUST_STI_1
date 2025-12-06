b633584d565ff787778411b1bf343300eace6cf2
lxl-git05 <XinLiu051024@outlook.com>
2025-12-07 00:26:35 +0800

    新建 简易示波器+函数发生器 STM32工程

---
+++ Personal/lxl/Func_IC/Func_IC/Top/Mymain.c
@@ -0,0 +1,51 @@
+#include "Mymain.h"
+
+int PWM_Output = 5000 ;
+
+void Mymain(void)
+{
+	// ******************* setup *******************
+	// 初始化
+	{
+		HAL_SYSTICK_Config(SystemCoreClock / 1000);
+		OLED_Init() ;
+		Serial_Init(&Serial_huart) ;
+		IC_Init() ;
+		PWM_Init(htim1 , TIM_CHANNEL_1) ;	// PWM输出
+		__enable_irq();	// ***全部初始化完毕后再开启Systick中断***
+	}
+	// *******************实验区域*******************
+
+	while(1)
+	{
+		// ******************* while *******************
+		// 测试按键功能
+		if (Key_Check(KEY_1 , KEY_SINGLE))
+		{
+			HAL_GPIO_TogglePin(LED0_GPIO_Port , LED0_Pin) ;
+		}
+		// ******************* 实验区域 *******************
+		PWM_SetCompare1(htim1 , TIM_CHANNEL_1 , PWM_Output ) ;
+		
+		IC_Capture_Update() ;
+		
+		
+		// 必须存在:OLED更新
+//		OLED_Update() ;
+	}
+}
+
+// Systick定时中断,1ms周期
+void HAL_SYSTICK_Callback(void)
+{
+	// 计时
+	static int count_sys = 0 ;
+	count_sys ++ ;
+	// 功能1: 按键
+	Key_Tick() ;
+	// 功能2:
+	if (count_sys % 1000 == 0)
+	{
+//		HAL_GPIO_TogglePin(LED0_GPIO_Port , LED0_Pin ) ;
+	}
+}
